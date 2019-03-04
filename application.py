f2661f3cb05fcaf08814a195cf6b7e8c26cb4108
laneboi <solid.formalin@gmail.com>
2019-03-04 22:40:21 +0300

    Roughly automated running of the service.

--- application.py
+++ application.py
@@ -4,7 +4,8 @@
 
 import json
 import os
-from flask import Flask, jsonify, render_template, request
+import platform
+from flask import Flask, jsonify, render_template, make_response, request
 from flask_session import Session
 
 # Enable ORM.
@@ -21,6 +22,21 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Set up database and database engine.
-os.environ['DATABASE_URL'] = None
+os.environ['DATABASE_URL'] = 'postgres://localhost:5432/'
 engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route('/')
+def index():
+    print('Hello world!')
+    return make_response()
+
+
+if __name__ == '__main__':
+    os.environ['FLASK_APP'] = 'application.py'
+    os.environ['FLASK_DEBUG'] = '1'
+    if platform.system() == 'Linux':
+        os.system(f'sudo flask run --no-reload')
+    elif platform.system() == 'Windows':
+        os.system(f'flask run --no-reload')

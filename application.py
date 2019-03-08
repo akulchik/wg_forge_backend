27d4b79391ed86f805c3880d5ab7e9fdcbd610b6
laneboi <solid.formalin@gmail.com>
2019-03-08 21:29:43 +0300

    Roughly implemented API cat method.

--- application.py
+++ application.py
@@ -5,7 +5,7 @@
 import json
 import os
 import platform
-from flask import Flask, jsonify, render_template, make_response, request
+from flask import Flask, render_template, make_response, request
 from flask_session import Session
 
 # Enable ORM.
@@ -22,20 +22,34 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Set up database and database engine.
-os.environ['DATABASE_URL'] = 'postgres://localhost:5432/'
+os.environ['DATABASE_URL'] = 'postgres://wg_forge:a42@localhost:5432/wg_forge_db'
 engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route('/')
-def index():
-    print('Hello world!')
-    return make_response()
+@app.route('/ping', methods=['GET'])
+def api_ping(*args, **kwargs):
+    resp = "Cats Service. Version 0.1"
+    return resp
 
 
-@app.route('/ping')
-def api_ping(methods=['GET'], *args, **kwargs):
-    return "Cats Service. Version 0.1"
+@app.route('/cats', methods=['GET'])
+def api_cats(*args, **kwargs):
+    _attribute = request.args.get('attribute', '1')
+    _order = request.args.get('order', 'ASC')
+    _offset = request.args.get('offset', '0')
+    _limit = request.args.get('limit', 'ALL')
+    resp = db.execute("""SELECT * FROM cats ORDER BY %s %s LIMIT %s OFFSET %s""" % (_attribute, _order, _limit, _offset)).fetchall()
+    return json.dumps([dict(r) for r in resp])
+
+
+@app.route('/cat', methods=['POST'])
+def api_add_cat(*args, **kwargs):
+    if request.method == 'POST':
+        cat_dict = request.get_json(force=True)
+        db.execute("""INSERT INTO cats (name, color, tail_length, whiskers_length)
+                          VALUES (:name, :color, :tail_length, :whiskers_length)""", cat_dict)
+        return "Everything just fine."
 
 
 if __name__ == '__main__':

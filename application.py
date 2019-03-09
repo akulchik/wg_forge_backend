9ac4e4f57b4f604b5383b97c204f97d779be403a
laneboi <solid.formalin@gmail.com>
2019-03-09 10:56:28 +0300

    Added limiter.

--- application.py
+++ application.py
@@ -6,8 +6,11 @@ import json
 import os
 import platform
 import werkzeug.exceptions as ex
-from flask import Flask, abort, render_template, make_response, request
-from flask_session import Session
+import wg_forge_api_schemas as _schemas
+from flask import Flask, abort, g, make_response, request
+from flask_expects_json import expects_json
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Enable ORM.
 from sqlalchemy import create_engine
@@ -19,8 +22,11 @@ app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
-# Initialize session.
-Session(app)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=['600 per minute']
+)
 
 # Set up database and database engine.
 os.environ['DATABASE_URL'] = 'postgres://wg_forge:a42@localhost:5432/wg_forge_db'
@@ -75,16 +81,42 @@ def api_cats(*args, **kwargs):
 
 
 @app.route('/cat', methods=['POST'])
+@expects_json(_schemas.CAT_SCHEMA, force=True)
 def api_add_cat(*args, **kwargs):
-    cat_dict = request.get_json(force=True)
-    print(cat_dict)
+    cat_dict = g.data
     db_cols = db.execute("""SELECT column_name
                                 FROM information_schema.columns
                                 WHERE table_name='cats'""").fetchall()
     column_names = [r[0] for r in db_cols]
+
     for k in cat_dict.keys():
         if k not in column_names:
             return abort(404)
+
+    _name = cat_dict.get('name')
+    if _name is None:
+        return abort(404)
+
+    _color = cat_dict.get('color')
+    if _color is None:
+        return abort(404)
+
+    _tail_length = cat_dict.get('tail_length')
+    if _tail_length is None:
+        return abort(404)
+    elif type(_tail_length) is not int:
+        return abort(404)
+    elif _tail_length < 0:
+        return abort(404)
+
+    _whiskers_length = cat_dict.get('whiskers_length')
+    if _whiskers_length is None:
+        return abort(404)
+    elif type(_whiskers_length) is not int:
+        return abort(404)
+    elif _whiskers_length < 0:
+        return abort(404)
+
     #db.execute("""INSERT INTO cats (name, color, tail_length, whiskers_length)
     #                 VALUES (:name, :color, :tail_length, :whiskers_length)""", cat_dict)
     return 'It still works.'

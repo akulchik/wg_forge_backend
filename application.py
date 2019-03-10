c604ac3085376054e622c598af4110d3a1a95c01
laneboi <solid.formalin@gmail.com>
2019-03-10 12:29:20 +0300

    Code refactoring and minor changes.

--- application.py
+++ application.py
@@ -25,8 +25,15 @@ limiter = Limiter(
     default_limits=['600 per minute']
 )
 
+
+def init_db():
+    os.environ['DATABASE_URL'] = 'postgresql://wg_forge:a42@localhost:5432/wg_forge_db'
+    engine = create_engine(os.getenv('DATABASE_URL'))
+    db = scoped_session(sessionmaker(bind=engine))
+
+
 # Set up database and database engine.
-os.environ['DATABASE_URL'] = 'postgres://wg_forge:a42@localhost:5432/wg_forge_db'
+os.environ['DATABASE_URL'] = 'postgresql://wg_forge:a42@localhost:5432/wg_forge_db'
 engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
@@ -37,7 +44,7 @@ def api_ping(*args, **kwargs):
     Returns heartbeat string with service information.
     :param args: Unexpected arguments.
     :param kwargs: Unexpected keyword arguments.
-    :return: "Cats Service. Version 0.1", 200 OK
+    :return: status code 200 - ok.
     """
     resp = "Cats Service. Version 0.1"
     return resp, 200
@@ -49,7 +56,8 @@ def api_cats(*args, **kwargs):
     Query database to retrieve data about cats.
     :param args: Unexpected arguments.
     :param kwargs: Unexpected keyword arguments.
-    :return: JSON with requested information OR 400 Bad Request
+    :return: status code 200 - JSON with requested information.
+    :return: status code 400 - invalid request parameters.
     """
     try:
         cat_dict = request.args
@@ -59,7 +67,7 @@ def api_cats(*args, **kwargs):
         limit = cat_dict.get('limit', 'ALL')
         offset = cat_dict.get('offset', '0')
         resp = db.execute(_q.select_cats_query.format(attribute, order, limit, offset)).fetchall()
-        return jsonify([dict(r) for r in resp])
+        return jsonify([dict(r) for r in resp]), 200
     except _exc.TooManyParameters as e:
         return abort(400, e.description)
     except _exc.UnexpectedParameter as e:
@@ -75,18 +83,22 @@ def add_cat(*args, **kwargs):
     Update database with a new cat.
     :param args: Unexpected arguments.
     :param kwargs: Unexpected keyword arguments.
-    :return: 201 Created OR 400 Bad Request
+    :return: status code 201 - database updated.
+    :return: status code 400 - bad JSON data.
     """
     try:
         cat_dict = g.data
+        _api.validate_no_extra_parameters(cat_dict)
         _api.validate_cat_tail_and_whiskers(cat_dict)
         #db.execute(_q.insert_cat_query, cat_dict)
         resp = 'Database successfully updated.'
         return resp, 201
     except _exc.TailLengthIsNegative as e:
         return abort(400, e.description)
+    except _exc.TooManyParameters as e:
+        return abort(400, e.description)
     except _exc.WhiskersLengthIsNegative as e:
         return abort(400, e.description)
 
 
-app.run(debug=True, use_reloader=False)
+app.run(debug=True, port=8080, use_reloader=False)
